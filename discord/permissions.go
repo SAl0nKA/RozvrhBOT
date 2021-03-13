cf1cb8c76c730e38ff5a7f4514b2d058f1ee739e
Niki <nklasakk@gmail.com>
2021-03-13 16:41:55 +0100

    Odstranene checkovanie IsDM

--- discord/permissions.go
+++ discord/permissions.go
@@ -1,20 +1,11 @@
 package discord
 
 import (
-	"github.com/bwmarrin/discordgo"
 	"github.com/SAl0nKA/RozvrhBOT/config"
 )
 
 func PermissionsCheck(Roles []string)bool{
 	if ContainsIDs(Roles, config.RoleIDs) || config.RoleIDSstring == "" {
-		return true
-	}
-	return false
-}
-
-func IsDM(Member *discordgo.Member)bool{
-	if Member == nil{
-		//s.ChannelMessageSend(m.ChannelID,"Tento príkaz je prístupny iba pre členov serveru s prislušnou rolou")
 		return true
 	}
 	return false
