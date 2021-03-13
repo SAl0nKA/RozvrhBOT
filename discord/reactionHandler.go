5b6d2b5d91499daa0a017efe1b292a35a83df3e9
Niki <nklasakk@gmail.com>
2021-03-13 21:37:05 +0100

    Odstranenie nepotrebnych podmienok a spriehladnenie kodu

--- discord/reactionHandler.go
+++ discord/reactionHandler.go
@@ -8,12 +8,12 @@ import (
 
 var Emojis = []string{"◀️", "▶️", "🔄", "❌"}
 
-//TODO reforma reakcii,je tu bordel
-//TODO bug pri nevyplnenych RolesIDs pri reakciach
+//krajšie to nebude
 func HandleReaction(s *discordgo.Session, r *discordgo.MessageReactionAdd) {
 	if r.UserID == s.State.User.ID {
 		return
 	}
+
 	var Rozvrh *RozvrhEmbed
 	member,_ := s.GuildMember(r.GuildID,r.UserID)
 	for _,rozvrh := range RozvrhEmbedy{
@@ -24,33 +24,36 @@ func HandleReaction(s *discordgo.Session, r *discordgo.MessageReactionAdd) {
 	if Rozvrh == nil {
 		return
 	}
-	if Rozvrh.MessageID == r.MessageID && Rozvrh.ChannelID == r.ChannelID && PermissionsCheck(member.Roles){
+
+	if PermissionsCheck(member.Roles){
 		channel, err := s.Channel(r.ChannelID)
 		if err != nil {
 			log.Println("Couldn't get the channel name: ", err)
 		}
+
 		switch r.Emoji.Name {
 		case "◀️":
 			log.Printf("[RozvrhBOT] User %s reacted with \"Previous day\" to rozvrh in channel %s",member.User.Username, channel.Name)
 			s.MessageReactionRemove(r.ChannelID,r.MessageID, "◀️",r.UserID)
+
 			embed := ReturnRozvrh(-1,r.MessageID)
 			s.ChannelMessageEditEmbed(r.ChannelID,r.MessageID,&embed)
 		case "▶️":
 			log.Printf("[RozvrhBOT] User %s reacted with \"Next day\" to rozvrh in channel %s", member.User.Username, channel.Name)
 			s.MessageReactionRemove(r.ChannelID,r.MessageID, "▶️",r.UserID)
+
 			embed := ReturnRozvrh(1,r.MessageID)
 			s.ChannelMessageEditEmbed(r.ChannelID,r.MessageID,&embed)
 		case "🔄":
-			//s.MessageReactionRemove(RozvrhChannelID,RozvrhMessageID, "🔄",r.UserID)
 			log.Printf("[RozvrhBOT] User %s reacted with \"Refresh\" to rozvrh in channel %s", member.User.Username, channel.Name)
 			s.ChannelMessageDelete(r.ChannelID,r.MessageID)
+
 			embed := ReturnRozvrh(0,"")
 			m, _ := s.ChannelMessageSendEmbed(r.ChannelID,&embed)
 			AddReactions(s,m.ChannelID, m.ID)
 			rozvrh := NewRozvrh(m.ChannelID, m.ID, m.GuildID,time.Now().Weekday())
 			RozvrhEmbedy = append(RozvrhEmbedy,rozvrh)
 		case "❌":
-			//s.MessageReactionRemove(r.ChannelID,r.MessageID, "❌",r.UserID)
 			log.Printf("[RozvrhBOT] User %s reacted with \"Delete\" to rozvrh in channel %s", member.User.Username, channel.Name)
 			s.ChannelMessageDelete(r.ChannelID,r.MessageID)
 		}
@@ -63,7 +66,7 @@ func AddReactions(s *discordgo.Session, channelid, messageid string){
 	for _,emoji := range Emojis{
 		err := s.MessageReactionAdd(channelid,messageid,emoji)
 		if err != nil {
-			log.Println(err)
+			log.Println("[RozvrhBOT] Error adding reactions:",err)
 		}
 	}
 }
